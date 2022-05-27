460a07c7e3ab854b9c738b5d1e9f7022fe85227d
trofimenk0 <trofimenkodpwork@gmail.com>
2022-05-27 19:10:55 +0200

    Remove comments

--- database.js
+++ database.js
@@ -4,18 +4,3 @@ const dbURI = process.env.DB_CONNECTION_URL;
 const dbClient = new MongoClient(dbURI);
 
 module.exports = dbClient;
-
-// async function run() {
-//   try {
-//     await dbClient.connect();
-//     const database = dbClient.db('rest_api_test');
-//     const posts = database.collection('posts');
-
-//     const query = { title: 'Updated title seconf time' };
-//     const movie = await posts.findOne(query);
-//     console.log(movie);
-//   } finally {
-//     await dbClient.close();
-//   }
-// }
-// run().catch(console.dir);
